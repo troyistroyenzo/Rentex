1c54306f1661de38a1d454e0c6c1c7a6f869692a
Troy Enzo <you@example.com>
2023-06-02 10:50:53 +0800

    removed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import Navbar from './components/NavBar.jsx'
 import Home from './components/Home.jsx'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Dashboard from './components/Dashboard.jsx'
@@ -10,7 +9,7 @@ function App() {
   return (
     <>
     <Router>
-      <Navbar />
+  
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/dashboard" element={<Dashboard />} />
